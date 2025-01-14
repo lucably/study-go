a4a93abb54278e30bb463689405e8a621fb5088c
Lucas Medeiros <lucably@hotmail.com>
2025-01-13 22:41:15 -0300

    http

---
+++ 22 - HTTP/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Olá Mundo!"))
+}
+
+func main() {
+
+	http.HandleFunc("/home", home)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+
+}
