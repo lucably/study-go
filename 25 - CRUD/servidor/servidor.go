e262df7f358bc983b119ccc6a9d32ab1c753a374
Lucas Medeiros <lucably@hotmail.com>
2025-01-23 17:42:51 -0300

    CRUD GET

--- 25 - CRUD/servidor/servidor.go
+++ 25 - CRUD/servidor/servidor.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type usuario struct {
@@ -60,4 +63,86 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	//STATUS CODES
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", idInserido)))
+}
+
+// BuscarUsuarios busca todos os usuários do DB.
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados!"))
+		return
+	}
+	defer db.Close()
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar os usuários"))
+		return
+	}
+	defer linhas.Close()
+
+	var usuarios []usuario
+	for linhas.Next() {
+		var usuario usuario
+
+		//  Scan => Pega cada linha e copia na ordem exata que veio.
+		if erro := linhas.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuário"))
+			return
+		}
+
+		usuarios = append(usuarios, usuario)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	//Transformando o slice em JSON.
+	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
+		w.Write([]byte("Erro ao converter os usuários em JSON"))
+		return
+	}
+}
+
+// BuscarUsuario busca um usuário do DB.
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+
+	//ParseUint => recebe 3 parametros: Variavel, baseNumero, TamanhoBit
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 32)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parâmetro para inteiro"))
+	}
+
+	//Abrir a conexao do banco aqui para ter a certeza que o usuario passou um parametro id.
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar ao DB."))
+		return
+	}
+
+	linha, erro := db.Query("select * from usuarios where id = ?", ID)
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar o usuário."))
+		return
+	}
+
+	var usuario usuario
+	if linha.Next() {
+		if erro := linha.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuário."))
+			return
+		}
+	}
+
+	//Caso tente buscar um usuario que não exista ele popula como default para ambos o parametro, como ID é int, com o valor 0, Nome string, com o valor "", Email string, com o valor ""
+	if usuario.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if erro := json.NewEncoder(w).Encode(usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para JSON!"))
+		return
+	}
 }
